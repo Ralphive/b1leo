632386b7a2c7d3753c613195d7720e7cc6555f09
Ralph <ralphrpo@gmail.com>
2018-02-27 10:49:38 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+/* Load NodeJS Modules */
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+
+var app = express();
+app.use(express.static('public'));
+
+
+//To Support body on post requests
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Root path to retrieve Index.html
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'views/index.html'));
+  });
+
+var port = process.env.PORT || 30000
+app.listen(port, function () {
+  console.log('Example app listening on port ' + port);
+});
