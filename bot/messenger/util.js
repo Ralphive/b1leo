ca28497b0ff57226de361b5bf28dbb4d36edcf8f
Ralph <ralphrpo@gmail.com>
2018-05-10 13:51:53 +0100

    Revert "Bot"

--- bot/messenger/util.js
+++
@@ -1,9 +0,0 @@
-exports.random = function (max)
-{
-    return Math.floor((Math.random() * max));
-};
-
-exports.encodeData = function(testData) {
-    let result = Buffer.from(JSON.stringify(testData)).toString('base64'); //btoa(JSON.stringify(testData)); 
-    return result;
-};
