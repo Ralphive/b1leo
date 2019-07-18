0518939575de36e1b004bb48426329169d9e36eb
Ralphive <ralphrpo@gmail.com>
2019-07-18 15:26:22 +0100

    add remote redis capability

--- smbmkt/app.js
+++ smbmkt/app.js
@@ -12,18 +12,48 @@ const biz = require('./modules/biz');
 const sql = require('./modules/sql');
 const start = require('./modules/start')
 
-/* Configure Redis */
-console.log("Configuring redis")
 var credentials = null;
 var vcap = null;
+
+/* Configure Redis */
+console.log("Configuring redis")
+
+//Check where the Redis instance will come from. 
+//From CF BackingServiecs, OR a Remote Host OR a local (credentials = null)
 if (process.env.VCAP_SERVICES) {
-    credentials = {}
     vcap = JSON.parse(process.env.VCAP_SERVICES);
-    credentials = vcap['redis'][0].credentials;
-    credentials.host = credentials.hostname
-    console.log("Redis credentials found in VCAP")
-};
-var redisClient = redis.createClient(credentials);
+  
+    if (vcap.hasOwnProperty('redis')) {
+      credentials = vcap.redis[0].credentials;
+      credentials.host = credentials.hostname
+      console.log("Redis credentials found in VCAP")
+    } else {
+      console.log("No Redis found in VCAP Services")
+    }
+  };
+  
+  if (!credentials) {
+    //Maybe Redis is on a remote enviroment
+    console.log("Looking for remote Redis connection details")
+    if (process.env.REDIS_HOST) {
+      console.log("trying to connect to Redis on " + process.env.REDIS_HOST)
+      credentials = {
+        host: process.env.REDIS_HOST,
+        port: process.env.REDIS_PORT,
+        password: process.env.REDIS_PASSWORD,
+  
+      }
+    } else {
+      console.log("No remote Redis details found, will try to connect locally")
+    }
+  }
+  
+  var redisClient = redis.createClient(credentials);
+  redisClient.on('error', function (er) {
+    console.trace('Here I am');
+    console.error(er.stack);
+  });
+
 redisClient.on('connect', function () {
     console.log("Connected to Redis")
     biz.setClient(redisClient)
